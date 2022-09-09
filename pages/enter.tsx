95b0a8f7b3988fc5d4e269f83ca269fad6e04c05
Frazic <bruylant.joshua@gmail.com>
2022-09-09 16:10:13 +0200

    Google Signin

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,7 +1,45 @@
+import Image from "next/image";
+import { auth, googleAuthProvider } from "../lib/firebase";
+
 export default function EnterPage({ }) {
+    const user = null;
+    const username = null;
+
+    /*
+    1. User signed out => Show SignInButton
+    2. User signed in but no username => Show UsernameForm
+    3. User signed in and has username => Show SignOutButton
+    */
     return (
         <main>
-            <h1>Sign Up</h1>
+            {
+                user ?
+                    username ? <SignOutButton /> : <UsernameForm />
+                    :
+                    <SignInButton />
+            }
         </main>
     )
+}
+
+// Sign in with Google Button
+function SignInButton() {
+    const signInWithGoogle = async () => {
+        await auth.signInWithPopup(googleAuthProvider);
+    };
+
+    return (
+        <button className="btn-google" onClick={signInWithGoogle}>
+            <img src="/google.png" alt="Google logo" width="30px" height="30px" style={{ marginRight: "10px" }} />Sign in with Google
+        </button>
+    )
+}
+
+// Sign out button
+function SignOutButton() {
+    return <button onClick={() => auth.signOut}>Sign Out</button>
+}
+
+function UsernameForm() {
+
 }
