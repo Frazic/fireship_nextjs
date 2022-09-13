74a886040fcbb8f593aa6691df20f0deaca1b391
Josh <jb1y21@soton.ac.uk>
2022-09-13 09:43:25 +0100

    Home page posts feed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,85 @@
-/* eslint-disable react/no-unescaped-entities */
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link';
-import toast from 'react-hot-toast';
+import { useState } from "react";
+import { firestore, postToJSON } from "../lib/firebase";
 
-import Loader from '../components/Loader';
+import PostFeed from "../components/PostFeed";
+import Loader from "../components/Loader";
+import firebase from "firebase";
 
-export default function Home() {
+// Max post to query per page
+const LIMIT = 1;
+
+// Have the server get the latest posts from firestore
+export async function getServerSideProps(context) {
+  const postsQuery = firestore
+    .collectionGroup('posts')
+    .where('published', '==', true)
+    .orderBy('createdAt', 'desc')
+    .limit(LIMIT);
+
+  const posts = (await postsQuery.get()).docs.map(postToJSON);
+
+  // Will be passed to the page component as props
+  return {
+    props: { posts },
+  };
+}
+
+export const fromMillis = firebase.firestore.Timestamp.fromMillis;
+
+export default function Home(props) {
+  // Use posts as state
+  const [posts, setPosts] = useState(props.posts);
+
+  // Loading posts state
+  const [loading, setLoading] = useState(false);
+
+  // State indicating we've reached the end of available posts
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  const getMorePosts = async () => {
+    setLoading(true);
+
+    // Get the last post from the current list to run a paginated query
+    const last = posts[posts.length - 1];
+
+    // To run paginated query we need a firestore timestamp
+    // Depending on where the last post was fetched (client or firestore) we might need to convert to firestore timestamp
+    const cursor = typeof last.createdAt === "number" ? fromMillis(last.createdAt) : last.createdAt;
+
+    // Build query
+    const query = firestore
+      .collectionGroup("posts")
+      .where("published", "==", true)
+      .orderBy("createdAt", "desc")
+      .startAfter(cursor)
+      .limit(LIMIT);
+
+    // Get data
+    const newPosts = (await query.get()).docs.map((doc) => doc.data());
+
+    // Add new posts to existing ones
+    setPosts(posts.concat(newPosts));
+    setLoading(false);
+
+    // If we get less than the limit then we've reached the end
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+  }
+
   return (
-    <div>
-      <Link prefetch={false} href={{
-        pathname: '/[username]',
-        query: { username: 'jeff' },
-      }}>
-        <a>Jeff's Profile</a>
-      </Link>
-      <div>
-        <Loader show></Loader>
-      </div>
-      <div>
-        <button onClick={() => toast.success("Hello toast!")}>
-          Toast
-        </button>
-      </div>
-    </div >
+    <main>
+      <PostFeed posts={posts} admin={false} />
+
+      {/* Show load more button if we're not loading and aren't at the end  */}
+      {!loading && !postsEnd && <button onClick={getMorePosts}>Load more</button>}
+
+      {/* Loader will be visible when loading=true */}
+      <Loader show={loading} />
+
+      {/* Show message when at end of posts */}
+      {postsEnd && "You've reached the end!"}
+
+    </main>
   );
 }
